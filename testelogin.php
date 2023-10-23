13f44aaad7f2e93ec2c705e949a93a03622cce60
LarissaProgramacao <146726190+LariProgamacao@users.noreply.github.com>
2023-10-23 00:27:51 -0300

    Update testelogin.php

--- testelogin.php
+++ testelogin.php
@@ -1,25 +1,26 @@
 <?php
-     
+session_start();
 
-     if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
-     {
-       include_once('conexao.php');
-       $email = $_POST['email'];
-       $senha = $_POST['semha'];
+if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+    include_once('conexao.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-       print_r('email: ' . $email);
-       print_r('senha: ' . $senha);
+    $sql = "SELECT * FROM cadastro WHERE email = '$email' AND senha = '$senha'";
+    $result = $mysqli->query($sql);
 
-     }
-     else
-     {
+    
+
+    if ($result->num_rows > 0) {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: refugepi.php');
+    } else {
+        $_SESSION['email'] = $email;
+        $_SESSION['senha'] = $senha;
         header('Location: index.php');
-     }
-
-
-
-
-
-
-
+    }
+} else {
+    header('Location: index.php');
+}
 ?>
