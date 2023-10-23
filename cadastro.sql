8474f5765764f91ea879c674cdabf62bbbe97be8
LarissaProgramacao <146726190+LariProgamacao@users.noreply.github.com>
2023-10-23 00:30:41 -0300

    Add files via upload

---
+++ cadastro.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/10/2023 às 05:25
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `refuge_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastro`
+--
+
+CREATE TABLE `cadastro` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefone` int(20) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `senha` varchar(55) NOT NULL,
+  `pais_origem` varchar(30) NOT NULL,
+  `data_nascimento` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cadastro`
+--
+
+INSERT INTO `cadastro` (`id`, `nome`, `email`, `telefone`, `sexo`, `senha`, `pais_origem`, `data_nascimento`) VALUES
+(5, 'Larissa Franco Rafael', 'larissarafael260205@gmail.com', 2147483647, 'feminino', 'Larissa11', 'Brasil', '2005-02-26'),
+(7, 'Ronaldo', 'ronaldo@gmail.com', 923482943, 'masculino', 'legal123', 'Rússia', '1994-03-08'),
+(8, 'Renato Almeida', 'xicrinha@gmail.com', 2147483647, 'masculino', 'Xicrinha123', 'Arábia Saudita', '2005-09-10');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cadastro`
+--
+ALTER TABLE `cadastro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cadastro`
+--
+ALTER TABLE `cadastro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
